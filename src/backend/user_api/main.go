c72b073de958e6c3c3599f74ae69c0b6406e7cac
kynmh69 <hiroki.kayanuma@gmail.com>
2024-02-17 10:46:49 +0900

    go mod

--- src/backend/user_api/main.go
+++ src/backend/user_api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -15,10 +14,6 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World")
 	})
-	port := 8080
-	host := "0.0.0.0"
-	addr := fmt.Sprintf("%s:%d", host, port)
 
-	e.Logger.Infof("Server is running at http://%s\n", addr)
-	e.Logger.Fatal(e.Start(addr))
+	e.Logger.Fatal(e.Start(":8080"))
 }
